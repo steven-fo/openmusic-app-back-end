d27e62e70e340cef0afd20db10a846f71b648adc
steven-fo <stevenfo0711@gmail.com>
2023-08-06 17:05:35 +0700

    forgot to add cache service to album service XD

--- src/server.js
+++ src/server.js
@@ -35,8 +35,11 @@ const _exports = require('./api/exports');
 const ProducerService = require('./services/rabbitmq/ProducerService');
 const ExportsValidator = require('./validator/exports');
 
+const CacheService = require('./services/redis/CacheService');
+
 const init = async () => {
-  const albumsService = new AlbumsService();
+  const cacheService = new CacheService();
+  const albumsService = new AlbumsService(cacheService);
   const songsService = new SongsService();
   const usersService = new UsersService();
   const authenticationsService = new AuthenticationsService();
