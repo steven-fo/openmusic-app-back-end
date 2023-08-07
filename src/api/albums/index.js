226e846df92db5c95581468cf39d58ad8df81570
steven-fo <stevenfo0711@gmail.com>
2023-08-07 22:31:36 +0700

    add storage service params

--- src/api/albums/index.js
+++ src/api/albums/index.js
@@ -4,8 +4,8 @@ const AlbumsHandler = require('./handler');
 module.exports = {
   name: 'albums',
   version: '1.0.0',
-  register: async (server, {service, validator}) => {
-    const albumsHandler = new AlbumsHandler(service, validator);
+  register: async (server, {service, storageService, validator}) => {
+    const albumsHandler = new AlbumsHandler(service, storageService, validator);
     server.route(routes(albumsHandler));
   },
 };
